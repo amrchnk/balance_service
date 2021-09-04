68a7ff6a7dab95117354d31dcde2fa1ece9a56ef
Anya Mirch <mirchenko1702@gmail.com>
2021-09-04 14:43:46 +0300

    fixes

--- pkg/repository/transactions_postgres.go
+++ pkg/repository/transactions_postgres.go
@@ -33,19 +33,19 @@ func (r *TransactionsPostgres) CreateTransaction(input models.Transaction)(strin
 func (r *TransactionsPostgres) GetAllTransactions(input models.AddressReq)([]models.Transaction,error){
     var tr_mas []models.Transaction
     s:=""
+    s+=fmt.Sprintf(" ORDER BY %s",input.Sort)
     switch {
         case input.Direction=="up":
             s+=" ASC"
         default:
             s+=" DESC"
     }
-    query:=fmt.Sprintf("SELECT user_id,type_t,amount,description,created FROM %s ORDER BY $1"+s, transactionsTable)
+    query:=fmt.Sprintf("SELECT user_id,type_t,amount,description,created FROM %s"+s, transactionsTable)
     fmt.Println(query)
     fmt.Println(input.Sort)
-    if err:=r.db.Select(&tr_mas,query,input.Sort); err!=nil{
+    if err:=r.db.Select(&tr_mas,query); err!=nil{
         fmt.Println(err)
         return nil,err
     }
-
     return tr_mas,nil
 }
